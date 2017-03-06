c920d2225ca27867e6a0e3693209280798827d94
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-03-06 18:11:19 -0500

    Add Submit link button
    and basic event listener

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
 }
