3f116a152d28016c08aee4d161c94a5b6a169c6b
Sergii Gatezh <gatezh@users.noreply.github.com>
2017-03-08 07:46:13 -0500

    Add article component
    Edit template and implement vote functions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent
   ],
   imports: [
     BrowserModule,
